afb4f1426b3f7578f03f33335b6da8601b3692a0
huangkai <18778335525@163.com>
2019-03-14 09:42:30 +0800

    链表结构

--- linked_list/linked_list.ts
+++ linked_list/linked_list.ts
@@ -1,67 +1,62 @@
-class Point<E> {
+class LinkNode<T> {
     /**
-     * 节点类型
+     * 链表节点类型
+     * @param data 节点数据
+     * @param next 此节点的下一个节点
      */
-    constructor(public e?: E,
-                public next?: Point<E>) {
+    constructor(
+        public data: T,
+        public next?: LinkNode<T>
+    ) {
     }
 }
 
 
-class LinkedList<E> {
+class LinkedList<T> {
     /**
      * 链表
      *    0  --> 1 --> 2 --> 3 --> 4 --> undefined
      *   |
      * head
      */
-    constructor(private size: number = 0,
-                private head?: Point<E>) {
-    }
-
-    getSize(): number {
-        // 获取链表元素个数
-        return this.size;
-    }
-
-    isEmpty(): boolean {
-        // 判断链表是否为空
-        return this.size === 0;
+    constructor(
+        private length: number,
+        private head?: LinkNode<T>
+    ) {
     }
 
 
-    addFirst(e: E): void {
-        // 链表头添加元素
-        const p = new Point(e, this.head);
-        this.size++;
-    }
-
-    /**
-     * 在链表的index(0-based)位置添加新的元素e
-     * 在链表中不是一个常用的操作
-     * @param index 位置
-     * @param e 元素
-     */
-    add(index: number, e: E): void {
-        if (index < 0 || index > this.size) {
+    // 添加元素
+    add(data: T, index: number): void {
+        if (index > this.length || index < 0) {
             throw new RangeError('Add failed, illegal index');
         }
-        let prevPoint = this.head;
-        // 因为有了dummyHead，多遍历一次，遍历index次
-        for (let i = 0; i < index - 1; i++) {
-            if (prevPoint === undefined) {
-                break;
-            } else {
-                prevPoint = prevPoint.next;
+
+        let prev;
+
+        if (this.head === undefined) {
+            this.head = new LinkNode(data);
+        } else {
+            prev = this.head;   // 初始定位到头节点
+            for (let i = 0; i < index - 1; i++) {
+                if (prev.next !== undefined) {
+                    prev = prev.next;
+                } else {
+                    break;
+                }
             }
         }
 
-        if (prevPoint !== undefined) {
-            prevPoint.next = new Point<E>(e, prevPoint.next);
+        if (prev !== undefined) {
+            prev.next = new LinkNode(data, prev.next);
         }
 
-        this.size++;
+        this.length++;
+    }
 
+    append(data: T): void {
+        // 链表尾插入元素
+        this.add(data, this.length);
     }
 
 
