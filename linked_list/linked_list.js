9480b0ec435419c436051083ebf4693854fb98bd
huangkai <18778335525@163.com>
2019-03-15 17:33:36 +0800

    链表结构

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -1,87 +1,76 @@
-class LinkNode<T> {
+var LinkNode = /** @class */ (function () {
     /**
      * 链表节点类型
      * @param data 节点数据
      * @param next 此节点的下一个节点
      */
-    constructor(
-        public data: T,
-        public next?: LinkNode<T>
-    ) {
+    function LinkNode(data, next) {
+        this.data = data;
+        this.next = next;
     }
-}
-
-
-class LinkedList<T> {
+    return LinkNode;
+}());
+var LinkedList = /** @class */ (function () {
     /**
      * 链表
      *    0  --> 1 --> 2 --> 3 --> 4 --> undefined
      *   |
      * head
      */
-    constructor(
-        private length: number,
-        private head?: LinkNode<T>
-    ) {
+    function LinkedList(length, head) {
+        this.length = length;
+        this.head = head;
     }
-
-
     // 添加元素
-    add(data: T, index: number): void {
+    LinkedList.prototype.add = function (data, index) {
         if (index > this.length || index < 0) {
             throw new RangeError('Add failed, illegal index');
         }
-
-        let prev;
-
+        var prev;
         if (this.head === undefined) {
             this.head = new LinkNode(data);
-        } else {
-            prev = this.head;   // 初始定位到头节点
-            for (let i = 0; i < index - 1; i++) {
+        }
+        else {
+            prev = this.head; // 初始定位到头节点
+            for (var i = 0; i < index - 1; i++) {
                 if (prev.next !== undefined) {
                     prev = prev.next;
-                } else {
+                }
+                else {
                     break;
                 }
             }
         }
-
         if (prev !== undefined) {
             prev.next = new LinkNode(data, prev.next);
         }
-
         this.length++;
-    }
-
-    append(data: T): void {
+    };
+    LinkedList.prototype.append = function (data) {
         // 链表尾插入元素
         this.add(data, this.length);
-    }
-
-    get(index: number): T {
+    };
+    LinkedList.prototype.get = function (index) {
         // 获得index位置的元素
         if (index < 0 || index >= this.length) {
             throw new RangeError('Get failed, illegal index');
         }
-
-        let cur = this.head;
-        for (let i = 0; i < index; i++) {
+        var cur = this.head;
+        for (var i = 0; i < index; i++) {
             if (cur !== undefined) {
                 cur = cur.next;
             }
         }
-
         if (cur !== undefined) {
             return cur.data;
-        } else {
+        }
+        else {
             throw new Error('Wrong Get func');
         }
-    }
-
-    contains(data: T): boolean {
+    };
+    LinkedList.prototype.contains = function (data) {
         // 是否存在data元素
-        let cur = this.head;
+        var cur = this.head;
         while (cur !== undefined) {
             if (cur.data === data) {
                 return true;
@@ -89,65 +78,46 @@ class LinkedList<T> {
             cur = cur.next;
         }
         return false;
-    }
-
-    remove(index: number): void {
+    };
+    LinkedList.prototype.remove = function (index) {
         // 移除index位置元素
         if (index < 0 || index >= this.length) {
             throw new RangeError('remove failed, illegal index');
         }
-
-        let prev = this.head;
-        for (let i = 0; i < index - 1; i++) {
+        var prev = this.head;
+        for (var i = 0; i < index - 1; i++) {
             if (prev !== undefined) {
                 prev = prev.next;
             }
         }
-
         if (prev !== undefined) {
-            let ret = prev.next as LinkNode<T>;
+            var ret = prev.next;
             prev.next = ret.next;
             ret.next = undefined;
             this.length--;
         }
-    }
-
-    getSize(): number {
+    };
+    LinkedList.prototype.getSize = function () {
         return this.length;
-    }
-
-    print(): void {
+    };
+    LinkedList.prototype.print = function () {
         // 打印链表
-        let arr = ['head'];
-        for (let i = 0; i < this.getSize(); i++) {
-            arr.push(`${this.get(i)}`);
+        var arr = ['head'];
+        for (var i = 0; i < this.getSize(); i++) {
+            arr.push("" + this.get(i));
         }
         arr.push('Null');
         console.log(arr.join(' -> '));
-    }
-}
-
-
-let linkedList = new LinkedList(0); // 构造一个空链表
-for (let i = 0; i < 5; i++) {
+    };
+    return LinkedList;
+}());
+var linkedList = new LinkedList(0); // 构造一个空链表
+for (var i = 0; i < 5; i++) {
     linkedList.append(i);
 }
-
 console.log(linkedList.getSize());
-
 linkedList.print();
-
 linkedList.remove(4);
-
 linkedList.remove(1);
-
 console.log(linkedList.contains(4));
 linkedList.print();
-
-
-
-
-
-
-
-
