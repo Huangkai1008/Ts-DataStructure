3e1cde24e643d949224591e3be2c39761773c52a
huangkai <18778335525@163.com>
2019-03-20 18:20:35 +0800

    最大堆部分实现

--- heap/max_heap.ts
+++ heap/max_heap.ts
@@ -26,4 +26,46 @@ class MaxHeap<T> {
         }
         return (index - 1) * 2;
     }
+
+    left(index: number): number {
+        // 给定index节点的左孩子节点
+        if (index <= 0) {
+            throw new RangeError('错误的序列号');
+        }
+        return (2 * index + 1);
+    }
+
+    right(index: number): number {
+        // 给定index节点的右孩子节点
+        if (index <= 0) {
+            throw new RangeError('错误的序列号');
+        }
+        return (2 * index + 2);
+
+    }
+
+    add(e: T) {
+        // 向最大堆中添加元素
+        this.data.push(e);
+        this.siftUp(this.data.length - 1);
+    }
+
+    private siftUp(index: number) {
+        // 当上浮元素大于父节点，继续上浮,并且不能上浮到0之上
+        // 直到index 等于 0 或 比 父亲节点小了
+        while (index > 0 && this.data[index] > this.data[this.parent(index)]) {
+            [this.data[index], this.data[this.parent(index)]] = [this.data[this.parent(index)], this.data[index]]; // 交换位置
+            index = this.parent(index);
+        }
+    }
+
+    findMax(): T {
+        // 获得堆中的最大元素
+        if (this.data.length === 0) {
+            throw new RangeError('空堆');
+        }
+        return this.data[0];
+    }
+
+
 }
